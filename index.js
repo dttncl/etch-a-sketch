43b2df9ac6b9cd19080871c6c16fad83c5f817de
dttncl <82695034+dttncl@users.noreply.github.com>
2023-06-20 17:39:17 -0400

    add sketching functionality

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const container = document.querySelector('#container');
 
+// create grid
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
     container.appendChild(div);
 }
+
+// function to fill color
+function addColor(e) {
+    e.preventDefault();
+    this.classList.add('fill');
+}
+
+// function to start sketching
+function startSketch(e) {
+    e.preventDefault();
+    const divs = document.querySelectorAll('#container > div');
+    console.log(divs)
+    for (let div of divs) {
+        div.addEventListener('mouseover', addColor)
+    }
+}
+
+// function to stop sketching
+function stopSketch(e) {
+    e.preventDefault();
+    const divs = document.querySelectorAll('#container > div');
+    for (let div of divs) {
+        div.removeEventListener('mouseover', addColor)
+    }
+}
+
+const sketch = document.querySelector('#container');
+sketch.addEventListener('mousedown',startSketch);
+sketch.addEventListener('mouseup',stopSketch);
